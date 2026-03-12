6fe0e36126fb7c368016694c4d14ba468d5fe0ed
lydia <lydia@lydiadeMac-mini.local>
2026-03-11 23:52:29 -0700

    fix + interpretation

--- src/rags/loader-and-splitter2.mjs
+++ src/rags/loader-and-splitter2.mjs
@@ -1,3 +1,8 @@
+/**
+ * terminal执行：node ./src/rags/loader-and-splitter2.mjs 
+ * 
+ */
+
 import "dotenv/config";
 import "cheerio";
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
@@ -70,7 +75,7 @@ for (const [qIdx, question] of questions.entries()) {
   const retrievedDocs = await retriever.invoke(question);
 
   // 使用similaritySearchWithScore获取相似度评分
-  const scoredResults = await vectorstore.similaritySearchVectorWithScore(question, 2);
+  const scoredResults = await vectorstore.similaritySearchWithScore(question, 2);
 
   // 打印检索到的文档和相似度评分
   console.log("\n【检索到的文档及相似度评分】");
@@ -103,3 +108,18 @@ for (const [qIdx, question] of questions.entries()) {
   console.log("\n");
 
 }
+
+
+// 1. 检索时候调用了两次检索，vectorStore.similaritySearchWithScore 返回了 pageContent 和评分，为什么还需要 retriever.invoke(question)
+// 确实。vectorStore.similaritySearchWithScore(question, 2) 是拿到问题最相关的两个文档和评分，
+// retriever.invoke(question) 是拿到最相关的文档，两者二选一即可。
+// 上节是为了演示各种 API 才这么写，下节开始直接调用 similaritySearchWithScore
+
+// 2. Puppeteerwebbaseloader和cheeriowebbaseloader的区别
+// PuppeteerWebBaseLoader：
+//    通过 puppeteer 启动一个真实的无头浏览器（Headless Browser）来加载网页 
+//    支持渲染动态网页。对于使用 React、Vue 等框架构建的、内容由 JS 动态加载的单页应用（SPA），它能拿到最终的完整 HTML
+// CheerioWebBaseLoader：
+//     用 cheerio 库在 Node.js 环境中解析HTML文本 。它不运行 JavaScript，只处理服务器返回的静态源代码。
+//    不支持渲染动态内容。如果网页内容是通过 JS 后来插入的，它无法抓取到
+
